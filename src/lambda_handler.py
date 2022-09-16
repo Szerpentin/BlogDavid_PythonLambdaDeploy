50bc4416f37ceed07d68c94ccc75c41a6f11dd46
DAVID-PC\david <david.bollobas@gmail.com>
2022-09-16 10:27:29 +0200

    init

---
+++ src/lambda_handler.py
@@ -0,0 +1,8 @@
+import datetime
+import json
+
+def handler(event, context) -> str:
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    utc_time = dt.replace(tzinfo=datetime.timezone.utc)
+
+    return str(utc_time)
