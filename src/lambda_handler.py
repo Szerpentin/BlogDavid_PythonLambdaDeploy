203986b35acd743a1a1a461038cc5a9fb75763bc
DAVID-PC\david <david.bollobas@gmail.com>
2022-09-16 15:56:12 +0200

    Lambda with API gateway

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,7 +1,16 @@
 import datetime
+import json
 
 def handler(event, context) -> str:
     dt = datetime.datetime.now(datetime.timezone.utc)
     utc_time = dt.replace(tzinfo=datetime.timezone.utc)
 
-    return str(utc_time)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "utc ": str(utc_time)
+        })
+    }
